71a2073f691e8692862f7a3a144975cb68e0e1c8
João Paulo Mesquita <jp-mesquita@live.com>
2023-03-20 16:57:38 -0300

    feat: create banner component

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -2,12 +2,14 @@ import '../../styles/globals.css'
 
 import { ReactNode } from 'react'
 import Header from '@/components/Header'
+import Banner from '@/components/Banner'
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html>
       <body>
         <Header />
+        <Banner />
         {children}
       </body>
     </html>
