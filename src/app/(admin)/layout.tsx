db2a4d5b8a3f83a5cf48e4ed85ecbf9c2c112d5d
João Paulo Mesquita <jp-mesquita@live.com>
2023-03-20 15:37:10 -0300

    feat: create invisible folder to just organize

---
+++ src/app/(admin)/layout.tsx
@@ -0,0 +1,11 @@
+import '../../styles/globals.css'
+
+import { ReactNode } from 'react'
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  )
+}
