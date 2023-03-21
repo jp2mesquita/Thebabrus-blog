644af4166036b81eb468a12b7b0d6b49a6aca09e
João Paulo Mesquita <jp-mesquita@live.com>
2023-03-21 10:36:27 -0300

    feat: add typing definitions to use sanity return

---
+++ typing.d.ts
@@ -0,0 +1,68 @@
+type Base = {
+  _createdAt: string
+  _id: string
+  _rev: string
+  _type: string
+  _updatedAt: string
+}
+
+interface Span {
+  _key: string
+  _type: 'span'
+  marks: string[]
+  text: string
+}
+
+interface Block {
+  _key: string
+  _type: 'block'
+  children: Span[]
+  markDefs: any[]
+  style: 'normal' | 'h1' | 'h2' | 'h3' | 'h4' | 'blockquote'
+}
+
+interface Reference {
+  _ref: string
+  _type: 'reference'
+}
+
+interface Image {
+  _type: 'image'
+  asset: Reference
+}
+
+interface Slug {
+  _type: 'slug'
+  current: string
+}
+
+interface Author extends Base {
+  bio: Block[]
+  image: Image
+  name: string
+  slug: Slug
+}
+
+interface Category extends Base {
+  description: string
+  title: string
+}
+
+export interface MainImage {
+  _type: 'image'
+  asset: Reference
+}
+
+export interface Title {
+  _type: 'string'
+  current: string
+}
+
+export interface Post extends Base {
+  author: Author
+  body: Block[]
+  categories: Category[]
+  mainImage: Image
+  title: string
+  description: string
+}
