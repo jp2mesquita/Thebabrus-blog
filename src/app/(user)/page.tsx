9f17ec7036c271a3d80b66a89caa0179841fd18f
João Paulo Mesquita <jp-mesquita@live.com>
2023-03-21 10:37:08 -0300

    feat: setup preview and create Bloglist file

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -1,12 +1,40 @@
 import { previewData } from 'next/headers'
+import { groq } from 'next-sanity'
+import { client } from '@/lib/sanity.client'
+import { PreviewSuspense } from 'next-sanity/preview'
+import PreviewBlogList from '@/components/PreviewBlogList'
+import BlogList from '@/components/BlogList'
 
-export default function Home() {
+const query = groq`
+  *[_type=='post'] {
+    ...,
+    author->,
+    categories[]->
+  } | order(_createdAt desc)
+`
+
+export default async function Home() {
   if (previewData()) {
-    return <div>Preview Mode</div>
+    return (
+      <PreviewSuspense
+        fallback={
+          <div role="status">
+            <p className="text-center text-lg animate-pulse text-[#f7ab0a]">
+              Loading Preview Data...
+            </p>
+          </div>
+        }
+      >
+        <PreviewBlogList query={query} />
+      </PreviewSuspense>
+    )
   }
+
+  const posts = await client.fetch(query)
 
   return (
     <div>
+      <BlogList posts={posts} />
       <h1>Not in Preview Mode</h1>
     </div>
   )
